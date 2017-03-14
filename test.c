bdb3c5bd1d37386bbee4ac0a31323908d0729b20
Caesurus <caesurus@gmail.com>
2017-03-13 22:24:46 -0400

    Initial commit

---
+++ test.c
@@ -0,0 +1,20 @@
+// Compile with:
+//    gcc -fno-stack-protector test.c -o test
+#include<stdio.h>
+#include<string.h>
+
+int updateMe = 0; 
+int main() 
+{
+  char buf[32];
+  gets(buf);
+
+  if(updateMe != 0) {
+    printf("Successfully overflowed the buffer! \n");
+  }
+  else {
+    printf("Try Again! \n");
+  }
+  return 0;
+}
+
